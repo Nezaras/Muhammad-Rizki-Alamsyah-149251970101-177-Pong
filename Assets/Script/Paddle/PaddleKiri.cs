6b3dcaedc917af696f12f571fd15f523664f6cf1
Nezaras <rizkialamsy@gmail.com>
2022-06-09 06:33:53 +0700

    Base Gameplay

---
+++ Assets/Script/Paddle/PaddleKiri.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleKiri : MonoBehaviour
+{
+    public int speed;
+    public KeyCode upKey;
+    public KeyCode downKey;
+    private Rigidbody2D rig;
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        // mencari input
+        Vector3 movement = GetInput();
+
+        // menggerakan object
+        MoveObject(movement);
+    }
+
+    private Vector2 GetInput()
+    {
+        if (Input.GetKey(upKey))
+        {
+            return Vector2.up * speed;
+        }
+        else if (Input.GetKey(downKey))
+        {
+            return Vector2.down * speed;
+        }
+
+        return Vector2.zero;
+    }
+
+    private void MoveObject(Vector2 movement)
+    {
+        rig.velocity = movement;
+    }
+}
